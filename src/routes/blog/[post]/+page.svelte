86ef29245b868fd5b7885e85d01c273d7a1c61b9
Claes Nymand Nilsson <claes89@gmail.com>
2024-04-05 23:01:46 +0200

    Add HTML comments

--- src/routes/blog/[post]/+page.svelte
+++ src/routes/blog/[post]/+page.svelte
@@ -12,16 +12,21 @@
   }
 </script>
 
+<!--Blog Header-->
 <div class="text-center w-full mb-12 mt-8">
+  <!--Category and description-->
   <p>
     <span class="text-blue-600 text-sm mr-2">
       {data.metadata.category.toUpperCase()}
     </span>{data.metadata.description}
   </p>
+
+  <!--Title and date-->
   <h1 class="font-[Kurale] text-7xl mt-3 mb-6">{data.metadata.title}</h1>
   <p class="text-slate-600">{formatDate(data.metadata.date)}</p>
 </div>
 
+<!--Blog Content-->
 <div class="markdown leading-relaxed max-w-4xl mx-auto">
   <svelte:component this={data.Component} />
 </div>
