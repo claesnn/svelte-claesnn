73f1f20edc172b308216ba30a7bb2677056c95c4
Claes Nymand Nilsson <claes89@gmail.com>
2024-04-06 00:58:52 +0200

    Add enhanced-images

--- src/routes/photography/+page.svelte
+++ src/routes/photography/+page.svelte
@@ -1,13 +1,18 @@
-<script>
-  const IMAGE_COUNT = 54
+<script lang="ts">
+  const images = import.meta.glob<string>("$lib/images/*-1280.webp", {
+    query: { enhanced: true },
+    import: "default",
+    eager: true,
+  })
 </script>
 
 <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-3">
-  {#each [...Array(IMAGE_COUNT)] as _, i}
+  {#each Object.values(images) as image, i}
     <a href={`/photography/${i}`}>
-      <img
-        src={`/images/${i}-640.webp?enhanced`}
-        alt=""
+      <enhanced:img
+        src={image}
+        alt="hobby"
+        loading={i < 6 ? "eager" : "lazy"}
       />
     </a>
   {/each}
